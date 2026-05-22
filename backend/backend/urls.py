423daef21122e4d335291bcdc4f8a802ecea27d2
mukundkhandelwal463 <mukundkhandelwal463@gmail.com>
2026-05-23 03:42:14 +0530

    fix: serve media files in prod and scale to 1 replica

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -42,4 +42,8 @@ urlpatterns = [
 ]
 
 if settings.MEDIA_URL and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.urls import re_path
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
